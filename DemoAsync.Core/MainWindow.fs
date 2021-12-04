15624e395a4a53b0c2a92b38e8c25c83418d53b4
Bent Tranberg <bent.tranberg@gmail.com>
2021-12-04 23:49:22 +0100

    Added progressbar

--- DemoAsync.Core/MainWindow.fs
+++ DemoAsync.Core/MainWindow.fs
@@ -8,16 +8,19 @@ open ElmishSupport
 type Model =
     {
         StatusText: string
+        Progress: int
     }
 
 type Msg =
     | DoSomeWork of Aos<Result<int,string>>
     | RunWithProgress
-    | UpdateStatusText of statusText:string
+    | UpdateStatus of statusText:string * progress:int
+    | WorkIsComplete // This message could carry a result from the work done.
 
 let initialModel =
     {
         StatusText = "Hello world!"
+        Progress = 0
     }
 
 let init () = initialModel, Cmd.none
@@ -40,17 +43,21 @@ let private doSomeWork event (m: Model) =
 let update (msg: Msg) (m: Model) : Model * Cmd<Msg> =
     match msg with
     | DoSomeWork event -> doSomeWork event m
-    | UpdateStatusText statusText ->
-        { m with StatusText = statusText }, Cmd.none
+    | UpdateStatus (statusText, progress) ->
+        { m with StatusText = statusText; Progress = progress }, Cmd.none
+    | WorkIsComplete ->
+        { m with StatusText = "Work was completed."; Progress = 0 }, Cmd.none
     | RunWithProgress ->
         let incrementDelayedCmd (dispatch: Msg -> unit) : unit =
             let delayedDispatch = async {
                 do! Async.Sleep 1000
-                dispatch (UpdateStatusText "One")
+                dispatch (UpdateStatus ("Early work", 30))
                 do! Async.Sleep 1000
-                dispatch (UpdateStatusText "Two")
+                dispatch (UpdateStatus ("Still working", 60))
                 do! Async.Sleep 1000
-                dispatch (UpdateStatusText "Three")
+                dispatch (UpdateStatus ("Late work", 90))
+                do! Async.Sleep 1000
+                dispatch WorkIsComplete
                 }
             Async.StartImmediate delayedDispatch
         { m with StatusText = "Started progress." }, Cmd.ofSub incrementDelayedCmd
@@ -58,6 +65,9 @@ let bindings () : Binding<Model,Msg> list = [
     "StatusText" |> Binding.oneWay (fun m -> m.StatusText)
     "DoSomeWork" |> Binding.cmd (DoSomeWork (Started ()))
     "RunWithProgress" |> Binding.cmd RunWithProgress
+    "Progress" |> Binding.oneWay (fun m -> float m.Progress)
     ]
 
-let designVm = ViewModel.designInstance initialModel (bindings ())
+let designTimeModel = { StatusText = "This is design time."; Progress = 30 }
+
+let designVm = ViewModel.designInstance designTimeModel (bindings ())
